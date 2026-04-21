1d874bcbec10e3b3d689c0945e6be15a1a71dc68
ericyam-git <70084832+ericyam-git@users.noreply.github.com>
2026-04-21 15:31:50 -0400

    Added 1 test

---
+++ src/test/java/SandboxTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SandboxTests {
+
+    @Test
+    public void Test_01() {
+        assertTrue(true);
+    }
+}
