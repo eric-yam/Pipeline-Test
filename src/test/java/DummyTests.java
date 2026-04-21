124c24351184b3605616b01adacb829486ebb0de
ericyam-git <70084832+ericyam-git@users.noreply.github.com>
2026-04-21 15:56:53 -0400

    Added surefire plugin in pom

--- src/test/java/DummyTests.java
+++ src/test/java/DummyTests.java
@@ -1,6 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DummyTests {
+
+    @Test
     public void ExpectedFailureTest() {
         assertTrue(false);
     }
