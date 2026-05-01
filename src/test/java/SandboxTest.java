4e9a9b9912773f1da49452ffe2643f0e5477f41e
ericyam-git <70084832+ericyam-git@users.noreply.github.com>
2026-05-01 10:46:05 -0400

    Get Env variables test

--- src/test/java/SandboxTest.java
+++ src/test/java/SandboxTest.java
@@ -11,6 +11,10 @@ public class SandboxTest {
 
     @Test
     public void Test_02() {
+        String u = System.getenv("DEV_USERNAME");
+        String p = System.getenv("DEV_PASSWORD");
 
+        System.out.println("Username: " + u);
+        System.out.println("Password: " + p);
     }
 }
